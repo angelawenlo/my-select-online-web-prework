2998e1fb2cd832c6668335e7b4618a1b50c92026
Angela Lo <angelawenlo@gmail.com>
2019-05-16 19:11:56 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  i = 0
  new_collection = []
 while i < collection.length
-  yield(collection[i]) == true
+  new_collection << yield(collection[i]) 
   i += 1
 end
 new_collection
